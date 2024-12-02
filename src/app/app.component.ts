3735182f1b06f39edff5ce4d124d4228dfeb3329
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-12-02 11:47:41 +0100

    ex2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: true,
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
   title = 'DOMiServerisAngularOscarMollinedoEchevarriaDAM1';
+  urlTret: string = ""
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    var contingut = document.getElementById('paragraf');
+    contingut!.innerText = contingut!.innerText.toUpperCase()
+
+    this.urlTret = document.URL
+
+  }
 }
+
+
